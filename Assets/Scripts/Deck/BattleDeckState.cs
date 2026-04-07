2a6d146cecf857729f66f33b94c12efcd84f478d
Jeon Jong Heon <122503945+skufmid@users.noreply.github.com>
2026-04-07 15:19:06 +0900

    [feat] Deck, PlayerDeck, BattleDeckState

---
+++ Assets/Scripts/Deck/BattleDeckState.cs
@@ -0,0 +1,53 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class BattleDeckState
+{
+    public Deck DrawPile { get; } = new();
+    public Deck Hand { get; } = new();
+    public Deck DiscardPile { get; } = new();
+    public Deck ExhaustPile { get; } = new();
+    
+    private const int MAX_HAND_SIZE = 10;
+    private const int DRAW_PER_TURN = 5;
+
+    public void InitializeFrom(PlayerDeck playerDeck)
+    {
+        DrawPile.Clear();
+        Hand.Clear();
+        DiscardPile.Clear();
+        ExhaustPile.Clear();
+
+        foreach (var card in playerDeck.Cards)
+        {
+            DrawPile.Add(card.CloneForBattle());
+        }
+
+        DrawPile.Shuffle();
+    }
+
+    public void DiscardPileToDrawPile()
+    {
+        foreach (var card in DiscardPile.Cards)
+        {
+            DrawPile.Add(card);
+        }
+        DiscardPile.Clear();
+        DrawPile.Shuffle();
+    }
+
+    public CardInstance DrawOne()
+    {
+        if (DrawPile.Count == 0)
+        {
+            DiscardPileToDrawPile();
+        }
+
+        if (DrawPile.Count == 0)
+            return null;
+
+        CardInstance card = DrawPile.RemoveAt(DrawPile.Count - 1);
+        Hand.Add(card);
+        return card;
+    }
+}
---
+++ Assets/Scripts/Deck/Deck.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using UnityEditor.Rendering;
+using UnityEngine;
+
+
+public class Deck
+{
+    protected readonly List<CardInstance> cards = new();
+
+    public IReadOnlyList<CardInstance> Cards => cards;
+    public int Count => cards.Count;
+
+    public virtual void Add(CardInstance card) => cards.Add(card);
+    public virtual bool Remove(CardInstance card) => cards.Remove(card);
+    public virtual void Clear() => cards.Clear();
+    public virtual CardInstance RemoveAt(int index)
+    {
+        CardInstance card = cards[index];
+        cards.RemoveAt(index);
+        return card;
+    }
+
+    public virtual void Shuffle()
+    {
+        // Fisher-yates shuffle
+        for (int i = Count - 1; i >= 0; i--)
+        {
+            int j = UnityEngine.Random.Range(0, i + 1);
+
+            var temp = cards[i];
+            cards[i] = cards[j];
+            cards[j] = temp;
+        }
+    }
+
+}
